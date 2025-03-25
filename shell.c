d1c02c70b2cd147ecc893acdafcc19a34da74f33
Arthur <arthur@cybrco.ai>
2025-03-25 17:08:53 -0400

    feat: adds parsing for commands and spawns child processes to execute them

--- shell.c
+++ shell.c
@@ -1,5 +1,8 @@
 #include "utils.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 /*
  * Appends a prefix each time a user is prompted for an input and retrieves the user's input.
@@ -15,7 +18,16 @@ int main() {
   size_t line_len = 0;
 
   while(poll_user_input("shortcut> ", &line, &line_len) > 0) {
-     printf(line, "\n");
+    Command *command = parse_command(line);
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+      execvp(command->program_name, command->args);
+      exit(0);
+    } else {
+      wait(NULL);
+    }
   }
 
   return 0;
--- utils.c
+++ utils.c
@@ -1,2 +1,41 @@
 #include "utils.h"
+#include <string.h>
+#include <stdlib.h>
 
+#define MAX_COMMAND_BYTES 1024
+#define DELIMITERS " \t\n\r"
+
+/*
+ * Returns the next non-empty token based on each byte in DELIMITERS. If no tokens remain, it
+ * returns NULL.
+ */
+char* next_non_empty_token(char** line) {
+  char* token;
+
+  while ((token = strsep(line, DELIMITERS)) && !*token);
+ 
+  return token;
+}
+
+Command* parse_command(char* command_str) {
+  char* command_str_copy = strndup(command_str, MAX_COMMAND_BYTES);
+  char* token;
+  int i = 0;
+
+  Command* command;
+  if (!(command = (Command*) malloc(sizeof(Command)))) {
+    return NULL;
+  }
+
+  if (!(command->args = (char**) calloc(MAX_COMMAND_BYTES, sizeof(char*)))) {
+    free(command);
+    return NULL;
+  }
+
+  while ((token = next_non_empty_token(&command_str_copy))) {
+    command->args[i++] = token;
+  }
+  command->program_name = command->args[0];
+
+  return command;
+}
